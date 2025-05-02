0f4c69b3a5373b4b49e1b91e3aad7b7e4bdef9d8
Peter A. <ink.splatters@pm.me>
2025-05-02 14:40:19 +0200

    refactored get_bearer_apple_silicon.sh

--- get_bearer_apple_silicon.sh
+++ get_bearer_apple_silicon.sh
@@ -1,45 +1,39 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
 set -e
 set -o pipefail
 
-if [[ ! "$(uname -a)" =~ ^Darwin.*arm64$ ]]; then
+_uname=$(uname)
+_arch=$(arch)
+
+if [[ $_uname != "Darwin" || $_arch != "arm64" ]]; then
 	cat <<'EOF'
-ERROR: this script requires:
-- macOS on Apple Silicon
-- Odido iOS app is installed and authenticated
+ERROR: this script requires macOS on Apple Silicon
 EOF
 	exit 1
 fi
 
-echo 'Please wait...'
-
-ODIDO_TOKEN=$(find ~/Library/Containers/**/nl.tmobile* \
-	-name 'Cache.db' \
-	-not -path '*crashlytics*' \
-	-exec sqlite3 {} 'select proto_props from cfurl_cache_blob_data ;' \; |
-	LC_ALL=C sed -E 's/.*([0-9a-f]{32}).*/\1/g' | tail -1)
-
-if [[ $ODIDO_TOKEN =~ [0-9a-f]{32} ]]; then
-
-	export ODIDO_TOKEN
-
-	_shell=$(basename -- "$SHELL")
-	_rc=.${_shell}rc
-	if [ "$_shell" = "fish" ]; then
-		_run="set -Ux ODIDO_TOKEN \$ODIDO_TOKEN"
-	else
-		_run="echo export ODIDO_TOKEN=\$ODIDO_TOKEN >> ~/$_rc"
-	fi
+if [[ "$1" = "" ]] || [[ "$1" =~ -h|--help ]]; then
 	cat <<EOF
-Done.
+Dumps odido.nl bearer token from Odido official app cache data
 
-ODIDO_TOKEN environment variable was set
-for persistence run:
+Usage:
+	$0 <[-h|--help] | [print]>
 
-$_run
+	-h|--help	prints this help message
+	print		dumps bearer token
+
+CAUTION: the token will be printed to stdout in plaintext!
+
 EOF
-else
-	echo 'ERROR: bearer token was not found. Make sure Odido app is installed and you are logged in'
 	exit 1
 fi
+
+if [ "$1" != "print" ]; then
+	echo "ERROR: command is invalid: $1"
+	exit 1
+fi
+
+/usr/bin/find ~/Library/Containers/*/Data/Library/Caches/nl.tmobile.mytmobile -name 'Cache.db' \
+	-exec /usr/bin/sqlite3 {} 'select proto_props from cfurl_cache_blob_data ;' \; |
+	LC_ALL=C sed -E 's/.*([0-9a-f]{32}).*/\1/g' | tail -1 | tr -d '\n'
