3bb1912891f3282a831c32c4a1d2ad5b676d0693
Peter A. <ink.splatters@pm.me>
2025-09-20 17:22:06 +0200

    linting and formatting

--- odido.py
+++ odido.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
-import requests
-import json
-from datetime import datetime
+import http.client as http_client
 import json
 import logging
-import colorlog
-import http.client as http_client
 import os
+
+import colorlog
+import requests
 
 # Credits
 # [Romkabouter430](https://tweakers.net/gallery/2749)
@@ -23,9 +22,7 @@ if __name__ == "__main__":
     http_client.HTTPConnection.debuglevel = 1
 
     handler = colorlog.StreamHandler()
-    handler.setFormatter(
-        colorlog.ColoredFormatter("%(log_color)s[%(levelname)s] %(message)s")
-    )
+    handler.setFormatter(colorlog.ColoredFormatter("%(log_color)s[%(levelname)s] %(message)s"))
 
     log = colorlog.getLogger()
     log.setLevel(logging.DEBUG)
@@ -35,9 +32,7 @@ if __name__ == "__main__":
         log.fatal("ODIDO_TOKEN environment variable is not set")
         exit(1)
 
-    threshold = int(
-        os.environ["ODIDO_THRESHOLD"] if "ODIDO_THRESHOLD" in os.environ else 1500
-    )
+    threshold = int(os.environ.get("ODIDO_THRESHOLD", 1500))
 
     accesstoken = os.environ["ODIDO_TOKEN"]
 
@@ -55,33 +50,29 @@ if __name__ == "__main__":
 
     dict = json.loads(response.content)
 
-    subscriptionUrl = dict["subscriptions"][0]["SubscriptionURL"]
-    response = requests.get(subscriptionUrl + "/roamingbundles", headers=headers)
+    subscription_url = dict["subscriptions"][0]["SubscriptionURL"]
+    response = requests.get(subscription_url + "/roamingbundles", headers=headers)
     check(response)
 
     dict = json.loads(response.content)
 
     data = {"Bundles": [{"BuyingCode": "A0DAY01"}]}
 
-    totalRemaining = 0
+    total_remaining = 0
 
     for bundle in dict["Bundles"]:
         if bundle["ZoneColor"] == "NL":
             remaining = bundle["Remaining"]
-            totalRemaining += remaining["Value"]
+            total_remaining += remaining["Value"]
 
     log.info(f"threshold: {threshold}")
-    if round(totalRemaining / 1024, 0) < threshold:
-        response = requests.post(
-            subscriptionUrl + "/roamingbundles", json=data, headers=headers
-        )
+    if round(total_remaining / 1024, 0) < threshold:
+        response = requests.post(subscription_url + "/roamingbundles", json=data, headers=headers)
         check(response)
         log.debug(response)
         log.info("2000MB aangevuld")
         # self.interval = int(self.args["interval"])
     else:
         log.info(
-            "There is "
-            + str(round(totalRemaining / 1024, 0))
-            + " MB remaining, no need to update"
+            "There is " + str(round(total_remaining / 1024, 0)) + " MB remaining, no need to update"
         )
