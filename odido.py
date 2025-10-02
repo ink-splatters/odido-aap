d8abed3335e1d6747bb31dad47607839723f3cd8
Peter A. <ink.splatters@pm.me>
2025-10-02 20:49:22 +0200

    * fixed minior issues * made linter satisfied

--- odido.py
+++ odido.py
@@ -1,50 +1,85 @@
 #!/usr/bin/env python
+import http.client
 import json
 import os
 import sys
+from typing import Any, Literal, overload
 
 import requests
 from loguru import logger
+from requests import Response
 
 # Credits
 # [Romkabouter430](https://tweakers.net/gallery/2749)
 # @Lyceris-chan (refactoring)
 
 
-def setup_logger():
+def get_required_var(name: str) -> str:
+    if name not in os.environ:
+        logger.error(f"required environment variable: {name} is not set")
+        exit(1)
+    return os.environ[name]
+
+
+user_id: str = get_required_var("ODIDO_USER_ID")
+access_token: str = get_required_var("ODIDO_TOKEN")
+threshold: int = int(os.environ.get("ODIDO_THRESHOLD", 1500))
+debug = int(os.environ.get("ODIDO_DEBUG", 0))
+
+
+def setup_logger() -> None:
     logger.remove()
     logger.add(
         sys.stderr,
-        level="INFO",  # increase verbosity: level="DEBUG",
-        backtrace=True,
-        diagnose=False,
+        level="DEBUG" if debug else "INFO",
+        # backtrace=True,
+        # diagnose=True
     )
 
     # wire-level logging
-    # http.client.HTTPConnection.debuglevel = 1
+    # WARNING: this exposes your credentials in plaintext
+    if debug != 0:
+        http.client.HTTPConnection.debuglevel = 1
 
 
-def check_and_update_data(accesstoken, threshold):
+def check_and_update_data() -> None:
     """
     Checks the Odido data balance and tops it up if below the threshold.
     """
 
-    def check_and_get_response_data(response):
+    @overload
+    def verify_get_response_data(
+        response: Response, require_json: Literal[True]
+    ) -> dict[str, Any]: ...
+
+    @overload
+    def verify_get_response_data(
+        response: Response, require_json: Literal[False] = False
+    ) -> dict[str, Any] | str: ...
+
+    def verify_get_response_data(
+        response: Response, require_json: bool = False
+    ) -> dict[str, Any] | str:
         """Helper function to check HTTP response status."""
         code = response.status_code
         if not response.ok:
-            reason = response.reason
-            logger.error(f"Request failed: {code} {reason}")
+            logger.error(f"Request failed: {code} {response.reason}")
             # Raise an exception instead of exiting to allow the main loop to continue
             response.raise_for_status()
 
-        data = response.json()
-        logger.debug(f"response: {json.dumps(data, indent=4)}")
+        try:
+            data = response.json()
+            logger.debug(f"response: {json.dumps(data, indent=4)}")
+        except ValueError:
+            data = response.text
+            if require_json:
+                raise ValueError(f"payload is not a json: {data}, but json is expected") from None
+            logger.debug(f"response: {data}")
         return data
 
     # Create new header with Authorization
     headers = {
-        "Authorization": "Bearer " + accesstoken,
+        "Authorization": f"Bearer {access_token}",
         "User-Agent": "T-Mobile 5.3.28 (Android 10; 10)",
         "Accept": "application/json",
     }
@@ -55,12 +90,12 @@ def check_and_update_data(accesstoken, threshold):
         headers=headers,
     )
 
-    data = check_and_get_response_data(response)
-    subscription_url = data["subscriptions"][0]["SubscriptionURL"]
+    data = verify_get_response_data(response, require_json=True)
+    subscription_url: str = data["subscriptions"][0]["SubscriptionURL"]
 
     logger.info("Fetching roaming bundle information...")
     response = requests.get(subscription_url + "/roamingbundles", headers=headers)
-    data = check_and_get_response_data(response)
+    data = verify_get_response_data(response, require_json=True)
 
     total_remaining = 0
     for bundle in data["Bundles"]:
@@ -75,29 +110,17 @@ def check_and_update_data(accesstoken, threshold):
         logger.warning("Data is below threshold. Attempting to top up...")
         data = {"Bundles": [{"BuyingCode": "A0DAY01"}]}
         response = requests.post(subscription_url + "/roamingbundles", json=data, headers=headers)
-        data = check_and_get_response_data(response)
+        data = verify_get_response_data(response)
         logger.info("Successfully requested 2000MB top-up.")
 
     else:
         logger.info("Data is sufficient. No action needed.")
 
 
-def get_required_var(name: str) -> str:
-    if name not in os.environ:
-        logger.fatal(f"required environment variable: {name} is not set")
-        exit(1)
-    return os.environ[name]
-
-
 if __name__ == "__main__":
     try:
         setup_logger()
 
-        user_id = get_required_var("ODIDO_USER_ID")
-        accesstoken = get_required_var("ODIDO_TOKEN")
-
-        threshold = int(os.environ.get("ODIDO_THRESHOLD", 1500))
-
-        check_and_update_data(accesstoken, threshold)
+        check_and_update_data()
     except Exception:
-        logger.opt(exception=True).debug("Debug error:")
+        logger.opt(exception=True).debug("Error:")
