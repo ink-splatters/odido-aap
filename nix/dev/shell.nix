d328cc93a0ff38df95eb57aacff2a6892000b4d7
Peter A. <ink.splatters@pm.me>
2025-11-22 16:39:26 +0100

    dev-shell improvements

--- nix/dev/shell.nix
+++ nix/dev/shell.nix
@@ -6,14 +6,20 @@
   }: let
     inherit (config) pre-commit craneLib;
   in {
-    devShells.default = craneLib.devShell {
-      inherit (config) checks;
+    devShells.default =
+      craneLib.devShell.override {
+        mkShell = pkgs.mkShell.override {
+          inherit (pkgs.llvmPackages_latest) stdenv;
+        };
+      } ({
+          inherit (config) checks;
 
-      packages = [pkgs.mdformat] ++ pre-commit.settings.enabledPackages;
+          packages = [pkgs.mdformat] ++ pre-commit.settings.enabledPackages;
 
-      shellHook = ''
-        ${pre-commit.installationScript}
-      '';
-    };
+          shellHook = ''
+            ${pre-commit.installationScript}
+          '';
+        }
+        // (builtins.removeAttrs config.commonArgsNative ["src" "stdenv"]));
   };
 }
