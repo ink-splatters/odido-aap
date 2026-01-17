a5d590dbf3dbd14b9d552249f13df55ffdfe781d
Peter A. <ink.splatters@pm.me>
2026-01-17 02:14:17 +0100

    rust-toolchain updated

--- nix/common/rust-toolchain.nix
+++ nix/common/rust-toolchain.nix
@@ -2,7 +2,17 @@
   perSystem = {inputs', ...}: {
     options.rust-toolchain = lib.mkOption {
       type = lib.types.attrs;
-      default = inputs'.fenix.packages.default.toolchain;
+
+      default = with inputs'.fenix.packages;
+      with default;
+        combine [
+          cargo
+          clippy
+          rust-std
+          rustc
+          rustc-unwrapped
+          rustfmt
+        ];
     };
   };
 }
